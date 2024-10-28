2f2374ecb61bc97b05cb658978700e0c937c79bd
pwkang <porweikang@hotmail.com>
2024-10-28 10:40:00 +0800

    chore: update details for npm package

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: getBase(),
 });
+
+function getBase() {
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://www.unpkg.com/' + packageJson.name + '@' + packageJson.version + '/dist/';
+  }
+}
