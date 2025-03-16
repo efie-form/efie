e362ae18edd053d8b0556026146feb2ec5066586
pwkang <porweikang@hotmail.com>
2025-03-16 22:39:39 +0800

    feat: add button to save schema

--- packages/builder/src/layouts/main-section/content-mode/FormJson.tsx
+++ packages/builder/src/layouts/main-section/content-mode/FormJson.tsx
@@ -3,7 +3,7 @@ import { useSchemaStore } from '../../../lib/state/schema.state';
 import JsonEditor from '../../../components/elements/JsonEditor';
 import { useSettingsStore } from '../../../lib/state/settings.state';
 import checkSchema from '../../../lib/checkSchema';
-import { FormFieldType } from '@efie-form/core';
+import { FormFieldType, type FormSchema } from '@efie-form/core';
 import Button from '../../../components/elements/Button';
 
 export default function FormJson() {
@@ -13,26 +13,34 @@ export default function FormJson() {
   const [isValidSchema, setIsValidSchema] = useState(false);
 
   useEffect(() => {
+    validateSchema(schema);
     setValue(JSON.stringify(schema, undefined, 2));
   }, [currentHistoryIndex]);
 
   const handleChange = (newSchema?: string) => {
     if (!newSchema) return;
     setValue(newSchema);
+    validateSchema(newSchema);
+  };
 
+  const handleSave = () => {
+    const schema = validateSchema(value);
+    if (!schema) return;
+    const firstPage = schema.form.fields.find(
+      (f) => f.type === FormFieldType.PAGE
+    );
+    if (firstPage) setPage(firstPage.id);
+    setSchema(schema);
+  };
+
+  const validateSchema = (data: string | FormSchema) => {
     try {
-      const newSchema = JSON.parse(value);
-
-      if (!checkSchema(newSchema)) return;
-
-      setSchema(newSchema);
-
-      const firstPage = newSchema.form.fields.find(
-        (f) => f.type === FormFieldType.PAGE
-      );
-      if (firstPage) setPage(firstPage.id);
+      const parsedSchema = typeof data === 'string' ? JSON.parse(data) : data;
+      const isValid = checkSchema(parsedSchema);
+      setIsValidSchema(isValid);
+      if (isValid) return parsedSchema;
     } catch {
-      // ignore
+      setIsValidSchema(false);
     }
   };
 
@@ -43,7 +51,14 @@ export default function FormJson() {
           <JsonEditor onChange={handleChange} value={value} />
         </div>
         <div className="flex justify-end h-[5%] items-center px-4">
-          <Button disabled={!isValidSchema}>Save</Button>
+          <Button
+            onClick={() => {
+              handleSave();
+            }}
+            disabled={!isValidSchema}
+          >
+            Save
+          </Button>
         </div>
       </div>
     </div>
