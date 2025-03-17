eceb4fbc09213ce5bc83a52dd2e2d537e52b36d0
pwkang <porweikang@hotmail.com>
2025-03-17 20:54:22 +0800

    feat: auto save if schema valid

--- packages/builder/src/layouts/main-section/content-mode/FormJson.tsx
+++ packages/builder/src/layouts/main-section/content-mode/FormJson.tsx
@@ -1,21 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useSchemaStore } from '../../../lib/state/schema.state';
-import JsonEditor from '../../../components/elements/JsonEditor';
 import { useSettingsStore } from '../../../lib/state/settings.state';
 import checkSchema from '../../../lib/checkSchema';
 import { FormFieldType, type FormSchema } from '@efie-form/core';
-import Button from '../../../components/elements/Button';
+import useDebounce from '../../../lib/hooks/useDebounce';
+import { motion } from 'framer-motion';
+import JsonEditor from '../../../components/elements/JsonEditor';
 
 export default function FormJson() {
   const { schema, currentHistoryIndex, setSchema } = useSchemaStore();
   const { setPage } = useSettingsStore();
-  const [value, setValue] = useState('');
-  const [isValidSchema, setIsValidSchema] = useState(false);
+  const [value, setValue] = useState('{}');
+  const [isValidSchema, setIsValidSchema] = useState(true);
+  const [internalIndex, setInternalIndex] = useState(currentHistoryIndex);
+  const [isUpdating, setIsUpdating] = useState(false);
 
   useEffect(() => {
+    if (isUpdating && internalIndex >= currentHistoryIndex) {
+      if (internalIndex === currentHistoryIndex) setIsUpdating(false);
+      return;
+    }
+    setIsUpdating(false);
     validateSchema(schema);
     setValue(JSON.stringify(schema, undefined, 2));
-  }, [currentHistoryIndex]);
+  }, [currentHistoryIndex, internalIndex, isUpdating]);
 
   const handleChange = (newSchema?: string) => {
     if (!newSchema) return;
@@ -25,13 +33,17 @@ export default function FormJson() {
 
   const handleSave = () => {
     const schema = validateSchema(value);
-    if (!schema) return;
+    if (!schema || !isChanged()) return;
+    setInternalIndex((prev) => prev + 1);
+    setIsUpdating(true);
     const firstPage = schema.form.fields.find(
       (f) => f.type === FormFieldType.PAGE
     );
     if (firstPage) setPage(firstPage.id);
     setSchema(schema);
   };
+
+  useDebounce(handleSave, 250, [value]);
 
   const validateSchema = (data: string | FormSchema) => {
     try {
@@ -44,23 +56,32 @@ export default function FormJson() {
     }
   };
 
+  const isChanged = () => {
+    const prevSchema = JSON.stringify(schema);
+    const newSchema = JSON.stringify(JSON.parse(value));
+    return prevSchema !== newSchema;
+  };
+
   return (
-    <div className="h-full flex flex-col">
-      <div className="bg-white rounded-md flex-1 w-full">
-        <div className="h-[95%]">
-          <JsonEditor onChange={handleChange} value={value} />
-        </div>
-        <div className="flex justify-end h-[5%] items-center px-4">
-          <Button
-            onClick={() => {
-              handleSave();
-            }}
-            disabled={!isValidSchema}
-          >
-            Save
-          </Button>
-        </div>
+    <div className="h-full flex flex-col relative overflow-hidden">
+      <div className="bg-white rounded-md flex-1 w-full h-full">
+        <JsonEditor onChange={handleChange} value={value} />
       </div>
+      <motion.div
+        className="absolute px-4 py-0.5 left-1/2 -translate-x-1/2 border border-danger bg-danger-50 text-danger-700 rounded-md"
+        initial={{
+          translateY: '100%',
+          bottom: -5,
+          opacity: 0,
+        }}
+        animate={{
+          translateY: isValidSchema ? '100%' : 0,
+          bottom: isValidSchema ? -5 : 16,
+          opacity: isValidSchema ? 0 : 1,
+        }}
+      >
+        <p className="typography-body2">Invalid Schema</p>
+      </motion.div>
     </div>
   );
 }
