9dd3e906c5fa594cda9a448a7739df6a94f9975b
pwkang <porweikang@hotmail.com>
2025-06-07 13:24:18 +0800

    chore: update file

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default [
     jsx: true,
   }),
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/*.d.ts'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/*.d.ts', '**/coverage/**'],
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
